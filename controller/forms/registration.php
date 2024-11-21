c929294cfcdb9cb3839daa731586ac52104abcdb
alcallegal02 <114919661+alcallegal02@users.noreply.github.com>
2024-11-21 17:54:54 +0100

    Fase 2
    Reestructuración de proyecto, registro de usuario conectado a la base de datos y css

---
+++ controller/forms/registration.php
@@ -0,0 +1,28 @@
+<?php
+include('../db/config.php');
+session_start();
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    $query = $connection->prepare("SELECT * FROM users WHERE EMAIL=:email");
+    $query->bindParam("email", $email, PDO::PARAM_STR);
+    $query->execute();
+    if ($query->rowCount() > 0) {
+        echo '<p class="error">¡La dirección de correo ya existe!</p>';
+    }
+    if ($query->rowCount() == 0) {
+        $query = $connection->prepare("INSERT INTO users(USERNAME,PASSWORD_HASH,EMAIL) VALUES (:username,:password_hash,:email)");
+        $query->bindParam("username", $username, PDO::PARAM_STR);
+        $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
+        $query->bindParam("email", $email, PDO::PARAM_STR);
+        $result = $query->execute();
+        if ($result) {
+            echo '<p class="success">¡Su registro ha sido completado!</p>';
+        } else {
+            echo '<p class="error">¡Algo fue mal!</p>';
+        }
+    }
+}
+?>
